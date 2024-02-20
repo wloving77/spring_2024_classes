fad5fc48f6168673f458f6348a57a21f88ff5e3b
William Loving <wloving77@gmail.com>
2024-02-19 23:58:46 -0500

    PL HWk done!

---
+++ programming_languages/hwk3/partner.ml
@@ -0,0 +1,214 @@
+(* Ben Life, Sinan Seslikaya, Jalen Wang, Michael Duru, William Loving - CS 4610 - HW 3B *)
+(* 
+The purpose of this homework is to get practice with basic OCaml, but especially 
+pattern matching and lists. Some functions will need to be recursive. Please use
+pattern matching where possible. As this assignment was produced with our course
+content in mind, please limit your use of OCaml functionality to what we have 
+been covering in class.
+
+Given below ar 2 versions of a function that you may refer to. The first version 
+uses the 'function' syntax, and the second uses the 'with' syntax. For this 
+assignment, try to use the 'function' syntax (the first example). If needed, you 
+may use the 'with' syntax style. 
+
+This function, is_first, takes an element and a list and determines if the element
+is the same as the first member of the list. These examples are here to help you 
+with syntax and style. 
+
+
+let is_first = function
+  | (_, []) -> false
+  | (a, x::_) -> a = x;;
+
+let is_first (a,lst)  = 
+  match lst with
+  | [] -> false
+  | x::_ -> a = x;;
+
+is_first(1,[1;2;3]);;
+is_first(1,[3;2;1]);;
+*)
+
+
+(*
+ * Return the list of all elements of list that are < e.
+ *  
+ * val less : 'a * 'a list -> 'a list 
+ *)
+
+let rec less = function
+    | (_, []) -> []
+    | (e, x::xs) -> if x < e then [x] @ less (e, xs) else less (e, xs);;
+
+less (5, []);;
+less (5, [1;10;100]);;
+less (-1, [-5; -1; 0]);;
+less (10, [-5; -1; 0]);;
+
+(*
+ * Write a tail-recursive version of `less`.
+ *  
+ * val less_tail : 'a * 'a list -> 'a list 
+ *)
+
+
+let rec less_tail_helper (acc, e, lst) = 
+    match lst with
+    | [] -> acc
+    | x::xs -> if x < e then less_tail_helper (acc @ [x], e, xs) else less_tail_helper (acc, e, xs);;
+
+
+let less_tail (e, lst) = less_tail_helper([], e, lst);;
+
+less_tail (5, []);;
+less_tail (5, [1;10;100]);;
+less_tail (-1, [-5; -1; 0]);;
+less_tail (10, [-5; -1; 0]);;
+
+(*
+ * Returns true if and only if list `list` contains adjacent equal elements.
+ *  
+ * val repeats : 'a list -> bool 
+ *)
+
+let rec repeats = function 
+    | [] -> false
+    | a::[] -> false
+    | a::b::cs -> (a = b) || repeats (b :: cs);;
+
+
+repeats([]);;
+repeats([1]);;
+repeats([1;1]);;
+repeats([1;1;2]);;
+repeats([1;2;3]);;
+repeats([1;2;3;4;5;6;7;7]);;
+
+
+
+(*
+ * Returns the value of polynomial `polynomial` at x.
+ * 
+ * We represent a polynomial as a list of float coefficients,
+ * with the constant coefficient first.
+ * 
+ * For example: 3x^2 + 5x + 1 would be represented as the list [1.0; 5.0; 3.0],
+ * and `eval 2.0 [1.0; 5.0; 3.0]` should evaluate to 23.0. 
+ * 
+ * For missing terms, 0.0 will be used. 
+ * i.e. x^3 - 2x is represented as the list [0.0; -2.0; 0.0; 1.0]
+ * 
+ * val eval : float * float list -> float 
+ *)
+
+let rec eval_helper = function 
+    | (x, p, []) -> 0.0
+    | (x, p, c::cs) -> ((x ** p) *. c) +. eval_helper(x, p+.1., cs)
+;;
+
+let eval (x, coefficients) = eval_helper (x, 0.0, coefficients);;
+
+eval(2.0, [1.0; 5.0; 3.0]);;
+eval(5.0, [0.0; -2.0; 0.0; 1.0]);;
+eval(0.0, [0.0; -2.0; 0.0; 1.0]);;
+
+(*
+ * Returns true if e is a member of list list.
+ *
+ * val is_member : 'a * 'a list -> bool
+ *)
+
+let rec is_member = function
+    | (_, []) -> false
+    | (e, a::[]) -> e = a
+    | (e, x::xs) -> e = x || is_member(e, xs);;
+
+
+is_member(1, [1;2;3]);;
+is_member(10, [1;2;3]);;
+is_member(1, []);;
+
+(* --- The following questions all build on each other --- *)
+
+(*
+ * Removes all the elements after the first occurrence of `e`
+ * if it is in the list `list`, otherwise returns the empty list. 
+ *
+ * val remove_after_e : 'a * 'a list -> 'a list 
+ *)
+
+let rec remove_after_e = function
+    | (_, []) -> []
+    | (e, x::xs) -> 
+         if x = e 
+         then [x] 
+         else (let rest = remove_after_e(e, xs) in
+              if rest = [] 
+              then [] 
+              else x::rest);;
+
+remove_after_e(5, [1;2;3;4]);;
+remove_after_e(5, [1;5;3;4]);;
+remove_after_e(5, []);;
+
+
+
+
+(*
+ * Removes the first occurrence of `e` from the list `list`
+ * if `a` is in the list, otherwise returns the unmodified list.
+ *  
+ * val remove_first : 'a * 'a list -> 'a list 
+ *)
+
+let rec remove_first = function
+    | (_, []) -> []
+    | (e, x::xs) -> if x = e then xs else [x] @ remove_first(e, xs);;
+
+
+remove_first(5, [1;2;3;4]);;
+remove_first(5, [1;5;3;4]);;
+remove_first(5, [1;5;5;4]);;
+remove_first(5, []);;
+
+(*
+ * Removes all the occurrences of e from the list list
+ * if e is in the list, otherwise returns the unmodified list.
+ *  
+ * val remove_all : 'a * 'a list -> 'a list 
+ *)
+
+
+let rec remove_all = function
+    | (_, []) -> []
+    | (e, x::xs) -> if x = e then remove_all(e, xs) else [x] @ remove_all(e, xs);;
+
+
+remove_all(5, [1;2;3;4]);;
+remove_all(5, [1;5;3;4]);;
+remove_all(5, [1;5;5;4]);;
+remove_all(5, []);;
+
+(*
+ * Removes all the elements of list `list` where `(test_fn element)`
+ * returns false, otherwise returns the unmodified list.
+ *
+ * *** Don't use List.filter
+ *
+ * val remove_if_not : ('a -> bool) * 'a list -> 'a list 
+ *)
+
+let rec remove_if_not = function
+    | (_, []) -> []
+    | (fn, x::xs) -> 
+        if fn (x) 
+        then [x] @ remove_if_not(fn, xs)
+        else remove_if_not(fn, xs) 
+;;
+
+let is_one a = a = 1;;
+
+remove_if_not (is_one, [2;3;4;5]);;
+remove_if_not (is_one, [1;3;4;5]);;
+remove_if_not (is_one, []);;
+remove_if_not (is_one, [1;1;1;1]);;
